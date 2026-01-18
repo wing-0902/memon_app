0e534f7e4e47273aca6537e5039e90c9f2d4317d
Wing <wing@me.wing.osaka>
2026-01-18 02:18:28 +0000

    とりあえず

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import license from 'rollup-plugin-license';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+	sveltekit(),
+	license({
+		thirdParty: {
+			output: path.resolve(__dirname, './static/oss-LICENSE.txt'),
+			includePrivate: false
+		}
+	})
+]
 });
