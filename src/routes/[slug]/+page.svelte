c30d08bbf27ab290e113332e2f743f411d222e10
Wing <wing@me.wing.osaka>
2026-01-18 10:19:09 +0000

    [skip ci]名前

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import { page } from '$app/state';
 
-  let slug = $derived(page.params.slug);
+  let targetId = $derived(page.params.slug);
+
+  import { itemStore } from '$lib/data/list.svelte';
+
+  const targetItem = $derived(
+        itemStore.items.find(i => i.id === targetId)
+    );
 </script>
 
 <div>
-  <p>現在のID：{slug}</p>
+  <h2>{targetItem.displayName}</h2>
+  <small>現在のID：{targetId}</small>
 </div>
