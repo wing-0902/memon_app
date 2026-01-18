c60ad10d6cb73b442c924aecc07ef60eb45b828e
Wing <wing@me.wing.osaka>
2026-01-18 10:22:17 +0000

    [skip ci] gotoって便利やね

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
   import { page } from '$app/state';
+  import { goto } from '$app/navigation';
 
   let targetId = $derived(page.params.slug);
 
   import { itemStore } from '$lib/data/list.svelte';
 
   const targetItem = $derived(itemStore.items.find((i) => i.id === targetId));
+
+  if (!targetItem) {
+    goto('/');
+  }
 </script>
 
 <div>
-  <h2>{targetItem.displayName}</h2>
-  <small>現在のID：{targetId}</small>
+  {#if targetItem}
+    <h2>{targetItem.displayName}</h2>
+    <small>現在のID：{targetId}</small>
+  {/if}
 </div>
