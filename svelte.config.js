54cca0db62cf0b2928e2cccc0138a5ca524dbf10
Wing <wing@me.wing.osaka>
2026-01-18 07:45:39 +0000

    ビルドを停止しない

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,13 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      handlehttperror: ({ path, referrer, message }) => {
+        console.warn(`Prerender Warning: ${message} at ${path} (linked from ${referrer})`);
+        return;
+      }
+    }
   },
   extensions: ['.svelte', '.md'],
   preprocess: [
