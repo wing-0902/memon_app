1032a98850a04e9e9f94ed546b3cbe646b5cd017
Wing <wing@me.wing.osaka>
2026-01-18 05:00:28 +0000

    [skip ci] ルートを整備

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
   <title>Welcome to Memon</title>
 </svelte:head>
 
-<h2>Wef</h2>
+<h2>ようこそ</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
