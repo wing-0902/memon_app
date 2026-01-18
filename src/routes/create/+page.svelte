a79e661a965f4aaaabc49aae98b46aea50cfec19
Wing <wing@me.wing.osaka>
2026-01-18 06:51:51 +0000

    verygood

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   let newName = $state('');
-  import itemStore from '$lib/data/list.svelte.ts';
-    import {goto} from '$app/navigation';
+  import { itemStore } from '$lib/data/list.svelte.ts';
+  import { goto } from '$app/navigation';
 
   let alreadyTrouble = $state(false);
 
   function handleClick() {
     if (newName) {
-        itemStore.addItem(newName);
-        goto('/');
+      itemStore.addItem(newName);
+      goto('/');
     } else {
-        alert('作成に失敗しました．');
-        alreadyTrouble = true;
+      alert('作成に失敗しました．');
+      alreadyTrouble = true;
     }
-  };
+  }
 </script>
 
 <div class="root">
@@ -21,5 +21,5 @@
     type="text"
     bind:value={newName}
   />
-  <button onclick={() => handleClick()} >作成</button>
+  <button onclick={() => handleClick()}>作成</button>
 </div>
