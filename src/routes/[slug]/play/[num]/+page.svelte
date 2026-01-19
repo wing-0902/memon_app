a3304e1cd0030cc59f390a1d54cafbcf05241bbe
Wing <wing@me.wing.osaka>
2026-01-19 12:33:11 +0000

    [skip ci] 初期化

---
+++ src/routes/[slug]/play/[num]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
+
+  let targetId = $derived(page.params.slug);
+  let quizNum = $derived(Number(page.params.num));
+
+  import { itemStore } from '$lib/data/list.svelte';
+  import { wordStore } from '$lib/data/words.svelte';
+
+  // itemStoreの存在？
+  const targetItem = $derived(itemStore.items.find((i) => i.id === targetId));
+  
+  if (!targetItem) {
+    goto('/');
+  }
+
+  // wordStore初期化
+  $effect(() => {
+    if (targetId) {
+      wordStore.load(targetId);
+    }
+  });
+
+  $effect(() => {
+    if (quizNum > wordStore.words.length) {
+      goto(`/${targetId}/play/complete/`)
+    }
+  })
+</script>
+
+<div class="root">
+  <p>{quizNum}問目</p>
+</div>
