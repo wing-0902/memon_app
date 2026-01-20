650f0b50d51c532edc713f3b7ca25c370f36f5b0
Wing <wing@me.wing.osaka>
2026-01-20 07:45:19 +0000

    [skip ci] 結果画面

--- src/routes/[slug]/play/complete/+page.svelte
+++ src/routes/[slug]/play/complete/+page.svelte
@@ -8,11 +8,18 @@
   import { page } from '$app/state';
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
+  import localforage from 'localforage';
 
   let targetId = $derived(page.params.slug);
   const targetItem = $derived(itemStore.items.find((i) => i.id === targetId));
 
-  onMount(() => {
+  // qFrom
+  let qFrom = $state<string | null>(null);
+  let totalQuizCount = $state<number>(0);
+
+  onMount(async () => {
+    qFrom = await localforage.getItem<string>('どっちからか');
+    totalQuizCount = await localforage.getItem<number>('テストの出題数') || 0;
     if (!targetItem) {
       goto('/');
       return;
@@ -28,6 +35,16 @@
 </script>
 
 <div>
-  {nowCorrectAnswers.list}
-  {nowCorrectAnswers.wrongList}
+  <h2>結果</h2>
+  <p>{targetItem?.displayName || ''}</p>
+  <small>計{totalQuizCount}問</small><br/>
+  {#if qFrom === 'omote'}
+    <small>おもて から</small>
+  {:else if qFrom === 'ura'}
+    <small>うら から</small>
+  {/if}
+  <br />
+
+  「正解{nowCorrectAnswers.list}」
+  「不正解{nowCorrectAnswers.wrongList}」
 </div>
