ac5c193e1e6788ad8561840bc79592893edf06af
Wing <wing@me.wing.osaka>
2026-01-28 13:21:08 +0000

    [skip ci]

---
+++ .idx/dev.nix
@@ -0,0 +1,40 @@
+{pkgs}: {
+  channel = "stable-24.11";
+  packages = [
+    pkgs.nodejs_22
+    pkgs.pnpm
+  ];
+  idx = {
+    extensions = [
+      "svelte.svelte-vscode"
+      "vue.volar"
+    ];
+    workspace = {
+      onCreate = {
+        pnpm-install = "pnpm install";
+      };
+      onStart = {
+        pnpm-install = "pnpm install";
+      };
+    };
+    previews = {
+      previews = {
+        web = {
+          command = [
+            "pnpm"
+            "run"
+            "dev"
+            "--host"
+            "0.0.0.0"
+            "--port"
+            "$PORT"
+          ];
+          manager = "web";
+          env = {
+            PORT = "5173"; 
+          };
+        };
+      };
+    };
+  };
+}
